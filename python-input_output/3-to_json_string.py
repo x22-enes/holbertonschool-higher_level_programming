4af9c96c954f1a7e1e5c535985aab5bd8d83962d
x22-enes <cenabbey@gmail.com>
2026-04-21 06:17:04 +0400

    Update and rename 3-write_file.py to 3-to_json_string.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains a function that returns the JSON representation
+of an object (string).
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object."""
+    return json.dumps(my_obj)
