b0f9b9a3993a8dc178db151690c35ac1fdc5cddf
x22-enes <cenabbey@gmail.com>
2026-04-21 06:17:39 +0400

    Update and rename 4-append_write.py to 4-from_json_string.py

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains a function that returns an object (Python data
+structure) represented by a JSON string.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """Returns an object represented by a JSON string."""
+    return json.loads(my_str)
