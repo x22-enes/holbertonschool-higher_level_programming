b8685facc4d1c46893312a6d3ed34aa75ebc4962
x22-enes <cenabbey@gmail.com>
2026-04-21 06:15:30 +0400

    Update and rename 1-number_of_lines.py to 1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains a function that writes a string to a text file
+(UTF8) and returns the number of characters written.
+"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file and returns char count."""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
