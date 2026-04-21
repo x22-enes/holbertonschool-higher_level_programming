725447e40ae018e73e5ed10764556219693dc692
x22-enes <cenabbey@gmail.com>
2026-04-21 06:05:52 +0400

    Update 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,5 +6,5 @@ of a class that inherited (directly or indirectly) from the specified class.
 
 
 def inherits_from(obj, a_class):
-    """Returns True if obj is a subclass of a_class, but not the exact class."""
+    """Returns True if obj is a true subclass of a_class."""
     return issubclass(type(obj), a_class) and type(obj) is not a_class
